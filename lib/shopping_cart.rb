cc02de615cbda534806b74ace4b6852188dd3e68
Drew Williams <drewwilliams5280@gmail.com>
2020-07-08 10:50:56 -0600

    Finishes Iteration 3, last assert_equal not quite right

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -16,7 +16,31 @@ class ShoppingCart
   def details
     {
       name: @name,
-      capacity: @capacity.tr("items", "").to_i 
+      capacity: @capacity.tr("items", "").to_i
     }
   end
+
+  def quantity_to_integer
+    @products.map do |product|
+      product.quantity.to_i
+    end
+  end
+
+  def total_number_of_products
+    quantity_to_integer.inject(0) {|sum, i| sum + i}
+  end
+
+  def is_full?
+    total_number_of_products > @capacity.tr("items", "").to_i
+  end
+
+  def products_by_category(category)
+    @products.map do |product|
+      if product.category == category
+        return product
+      end
+      @products 
+    end
+  end
+
 end
